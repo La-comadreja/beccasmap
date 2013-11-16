067a0ed38f875dd329253c45ba2d5194e1be1fd0
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-11-16 11:57:44 -0500

    Initialize empty Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitguide::Application.config.secret_key_base = 'a65acb047b9a6575c19830b1b481f6ed7bbbede0fc237b1c75b4bf8a028013397f1f085f1017fc3f750f869621e81053c5b7317fc67c8e6af623c7304f5cf91d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitguide::Application.config.session_store :cookie_store, key: '_gitguide_session'
