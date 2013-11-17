eebf6fc3da9fca4d1c57e2f1b29aa74e32d7bf87
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-11-17 15:05:27 -0500

    Dates on locations page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   end
 
   def locations
-    page = Nokogiri::HTML(open(URI.escape("http://www.garysguide.com/events")))
+    page = Nokogiri::HTML(open("app/views/home/garysguide.html"))
+    #page = Nokogiri::HTML(open(URI.escape("http://www.garysguide.com/events")))
+ 
+    @dates = page.css(".boxx") 
+    @locations = page.css(".freg")
   end
 end
