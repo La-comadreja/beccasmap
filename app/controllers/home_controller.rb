0e852b9a90b2568391e29eecbad2b4074842cd9d
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-11-17 15:08:51 -0500

    Check if date string includes "Week of"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,12 @@ class HomeController < ApplicationController
     #page = Nokogiri::HTML(open(URI.escape("http://www.garysguide.com/events")))
  
     @dates = page.css(".boxx") 
-    @locations = page.css(".freg")
+    cal = ""
+    @dates.each do |d|
+      if d.include? "Week of"
+        cal = d
+        break
+      end
+    end
   end
 end
