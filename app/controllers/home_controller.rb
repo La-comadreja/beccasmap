201147e2b50639685b88f92089a0ff5dd8d7986d
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-11-17 16:47:39 -0500

    Got pure text for the locations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,20 @@ class HomeController < ApplicationController
   def locations
     page = Nokogiri::HTML(open("app/views/home/garysguide.html")) 
     @sections = page.css(".boxx")
-    @day = "THIS DOESNT WORK"
-    
+    @i = -1
+
     @sections.each do |s|
       if s.to_s.include? "Week of"
         @day = s.to_s.split("<font class=\"flarge\"><b>")[2]
+        @places = s.css(".freg")
+        break
+      end
+    end
+
+    @places.each do |p|
+      if @day.include? p.to_s
+        @i += 1
+      else
         break
       end
     end
