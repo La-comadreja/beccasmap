d404d32f8122a2e309333c00225349d3a66d1712
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-12-27 00:45:50 -0500

    Constraints on events

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -9,4 +9,15 @@ FactoryGirl.define do
     price "0.00"
     description "Book Club! Review arrays, strings and sorting Chapter 1"
   end
+  
+  factory :event2, class: Event do
+    category "Professional Development"
+    datetime "2014-10-02 19:00:00"
+    name "Problem Solving Practice for Coders"
+    venue "Dress for Success Hudson County"
+    address "121 Newark Avenue, Jersey City"
+    link "http://www.dressforsuccess.org/affiliate.aspx?sisid=32&pageid=3"
+    price "10.00"
+    description "Business casual is a somewhat baffling phrase to the uninitiated. Is it businesswear, or is "
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -11,6 +11,13 @@ describe Event do
     expect(@event).to be_invalid
   end
   
+  it "requires category to be under 100 characters" do
+    @event.category = "A" * 100
+    expect(@event).to be_valid
+    @event.category << "A"
+    expect(@event).to be_invalid
+  end
+  
   it "requires a datetime" do
     @event.datetime = nil
     expect(@event).to be_invalid
@@ -21,9 +28,23 @@ describe Event do
     expect(@event).to be_invalid
   end
   
+  it "requires event name to be under 100 characters" do
+    @event.name = "A" * 100
+    expect(@event).to be_valid
+    @event.name << "A"
+    expect(@event).to be_invalid
+  end
+  
   it "allows venue to be nil" do
     @event.venue = nil
     expect(@event).to be_valid
+  end
+  
+  it "requires non-nil venue name to be under 100 characters" do
+    @event.venue = "A" * 100
+    expect(@event).to be_valid
+    @event.venue << "A"
+    expect(@event).to be_invalid
   end
   
   it "requires an address" do
@@ -31,8 +52,22 @@ describe Event do
     expect(@event).to be_invalid
   end
   
+  it "requires address to be under 200 characters" do
+    @event.address = "A" * 200
+    expect(@event).to be_valid
+    @event.address << "A"
+    expect(@event).to be_invalid
+  end
+  
   it "requires a link" do
     @event.link = nil
+    expect(@event).to be_invalid
+  end
+  
+  it "requires link to be under 1000 characters" do
+    @event.link = "A" * 1000
+    expect(@event).to be_valid
+    @event.link << "A"
     expect(@event).to be_invalid
   end
   
@@ -44,5 +79,45 @@ describe Event do
   it "allows description to be nil" do
     @event.description = nil
     expect(@event).to be_valid
+  end
+  
+  it "requires non-nil description to be under 500 characters" do
+    @event.description = "A" * 500
+    expect(@event).to be_valid
+    @event.description << "A"
+    expect(@event).to be_invalid
+  end
+  
+  context "unique fields" do
+    before(:each) do
+      @event2 = FactoryGirl.build(:event2)
+      @event.save
+    end
+    
+    it "does not allow venue address + event name + event date/time to be case-insensitive duplicated" do
+      expect(@event2).to be_invalid
+      @event2.name = @event.name.upcase
+      @event2.address = @event.address.upcase
+      expect(@event2).to be_invalid
+    end
+    
+    it "allows event name + date/time duplicated if venue address is case-insensitive different" do
+      @event2.address = @event.address.upcase
+      expect(@event2).to be_invalid
+      @event2.address = "37 West 28th Street, 5th Floor"
+      expect(@event2).to be_valid
+    end
+    
+    it "allows venue address + event date/time duplicated if event name is case-insensitive different" do
+      @event2.name = @event.name.upcase
+      expect(@event2).to be_invalid
+      @event2.name = "What is Business Casual?"
+      expect(@event2).to be_valid
+    end
+    
+    it "allows venue address + event name duplicated if event date/time is different" do
+      @event2.datetime = "2014-10-03 19:00:00"
+      expect(@event2).to be_valid
+    end
   end
 end
